014fd3a4a3c9adb0cbebdf68b0b0725e7a3800bc
shammon5787 <sadam.bsit@gmail.com>
2024-12-09 14:15:52 +0500

    adds

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,6 +5,8 @@ import { useSelector } from 'react-redux'
 const Card = () => {
     const [activeCard, setactiveCard] = useState(true)
     const selectItem = useSelector((state)=>state.card.card)
+    const totalitem = selectItem.reduce((totalitem, item)=>totalitem + item.qty, 0)
+    const totalprice = selectItem.reduce((totalprice, item)=>totalprice + item.qty * item.price, 0)
     return (
         <>
             <div className={`bg-zinc-800 w-full h-full fixed top-0 right-0 lg:w-[28vw] p-3 ${activeCard ? "translate-x-0" : "translate-x-full"} transition-all duration-500 z-50 `}>
@@ -22,8 +24,8 @@ const Card = () => {
                 }
 
                 <div className='fixed bottom-3'>
-                    <h1>Total Items: 0</h1>
-                    <h1>Total Price: 0</h1>
+                    <h1>Total Items: {totalitem}</h1>
+                    <h1>Total Price: {totalprice}</h1>
                     <hr />
                     <button className='px-2 rounded-md font-semibold transition-all duration-500 bg-blue-950 my-2  w-[90vw] lg:w-[26vw] '>Add To Card</button>
                 </div>
